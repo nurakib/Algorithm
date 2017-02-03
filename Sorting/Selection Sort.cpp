53dcaa782fcf71836e2e813defeda2f9e6fa326d
Nayem Uddin <nurakib.nstu@gmail.com>
2017-02-04 01:13:41 +0600

    O(n^2)

---
+++ Sorting/Selection Sort.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+void Swap(int *xp, int *yp){
+    int tmp = *xp;
+    *xp = *yp;
+    *yp = tmp;
+}
+void selectionSort(int arr[], int n){
+    int i, j, min_idx;
+    for(i = 0; i < n - 1; i++){
+        min_idx = i;
+        for(j = i + 1; j < n; j++){
+            if(arr[j] < arr[min_idx])
+                min_idx = j;
+        }
+        Swap(&arr[min_idx], &arr[i]);
+    }
+}
+void printArray(int arr[], int sz){
+    for(int i = 0; i < sz; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
+int main(){
+    int arr[] = {3, 2, 7, 9, 11, 1, 6};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    selectionSort(arr, n);
+    printf("Sorted Array: ");
+    printArray(arr, n);
+    return 0;
+}
