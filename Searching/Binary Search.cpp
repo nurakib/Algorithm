a3030524f6d91f425f1f7bf95771afa9fc3777ab
Nayem Uddin <nurakib.nstu@gmail.com>
2017-05-01 21:54:40 +0600

    Update Binary Search.cpp

--- Searching/Binary Search.cpp
+++ Searching/Binary Search.cpp
@@ -11,7 +11,7 @@ int binarySearch(int arr[], int l, int r, int x){
         if (arr[mid] > x) return binarySearch(arr, l, mid-1, x);
 
         // Else the element can only be present in right subarray
-        return binarySearch(arr, mid+1, r, x);
+        else return binarySearch(arr, mid+1, r, x);
    }
    
    // We reach here when element is not present in array
